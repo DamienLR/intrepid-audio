8b8198e0846c95e9425d4e4cf8ca085b79c7ae10
Damien <damienroot@icloud.com>
2023-10-16 18:58:55 +0100

    create Footer component

---
+++ frontend/src/app/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandFacebook,
+} from "@tabler/icons-react";
+
+export default function Footer() {
+  return (
+    <footer className=" bg-slate-950">
+      <div className="container flex flex-col p-4 mx-auto md:flex-row md:justify-between">
+        <div className="flex justify-center gap-4 md:gap-6">
+          {/* #f1f5f9 is the same as text-slate-100 */}
+          {/* mr-1 because the facebook logo has weird spacing and this makes things more uniform */}
+          <IconBrandInstagram
+            className="mr-1 hover:cursor-pointer hover:fill-slate-600"
+            color="#f1f5f9"
+          />
+          <IconBrandTwitter
+            className="hover:cursor-pointer hover:fill-slate-600"
+            color="#f1f5f9"
+          />
+          <IconBrandFacebook
+            className="hover:cursor-pointer hover:fill-slate-600"
+            color="#f1f5f9"
+          />
+        </div>
+        <p className="mt-3 text-center md:mt-0 text-slate-100">
+          &copy; <span className="font-extralight">Intrepid Audio</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
