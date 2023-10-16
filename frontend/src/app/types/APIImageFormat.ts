74571b69e51fcbd3220337fc7a446cadd7d5f1cb
Damien <damienroot@icloud.com>
2023-10-16 01:48:37 +0100

    create APIImageFormat type

---
+++ frontend/src/app/types/APIImageFormat.ts
@@ -0,0 +1,13 @@
+type APIImageFormat = {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path: null | string;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+};
+
+export default APIImageFormat;
