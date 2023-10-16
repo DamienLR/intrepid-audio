bb45134e967e52aaecb7fc20918058e554933598
Damien <damienroot@icloud.com>
2023-10-16 01:50:07 +0100

    create ResponseWrapper type

---
+++ frontend/src/app/types/ResponseWrapper.ts
@@ -0,0 +1,13 @@
+type ResponseWrapper<T> = {
+  data: T;
+  meta: {
+    pagination: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    };
+  };
+};
+
+export default ResponseWrapper;
