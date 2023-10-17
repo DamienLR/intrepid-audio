2b37f50d6f78bab188a333c75c8c87015d8f55b1
Damien <damienroot@icloud.com>
2023-10-17 01:10:59 +0100

    modify artist controller to use slug

--- backend/src/api/artist/controllers/artist.ts
+++ backend/src/api/artist/controllers/artist.ts
@@ -2,6 +2,20 @@
  * artist controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::artist.artist');
+export default factories.createCoreController(
+  "api::artist.artist",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.db.query("api::artist.artist").findOne({
+        where: { slug: id },
+        populate: ["releases", "portrait"],
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
