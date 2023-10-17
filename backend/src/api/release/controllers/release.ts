554b00834a59378e612ac58c4d21ddfbb4d55cd0
Damien <damienroot@icloud.com>
2023-10-17 01:10:35 +0100

    modify release controller to use slug

--- backend/src/api/release/controllers/release.ts
+++ backend/src/api/release/controllers/release.ts
@@ -2,6 +2,20 @@
  * release controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::release.release');
+export default factories.createCoreController(
+  "api::release.release",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.db.query("api::release.release").findOne({
+        where: { slug: id },
+        populate: ["artists", "coverArt"],
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
