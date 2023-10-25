c88a1ebc4be3b9dd32c3ea29a6ff4af10a856ee7
Damien <damienroot@icloud.com>
2023-10-25 04:38:50 +0100

    remove SectionDivider custom header styles

--- frontend/src/app/components/ui/SectionDivider.tsx
+++ frontend/src/app/components/ui/SectionDivider.tsx
@@ -5,7 +5,7 @@ type SectionDividerProps = {
 export default function SectionDivider({ text }: SectionDividerProps) {
   return (
     <div className="flex items-center mx-auto">
-      <h2 className="mr-6 text-2xl font-bold uppercase md:mr-8 text-slate-950 md:text-3xl whitespace-nowrap">
+      <h2 className="mr-6 font-bold uppercase md:mr-8 text-slate-950  whitespace-nowrap">
         {text}
       </h2>
       <hr className="w-full h-0.5 bg-slate-950" />
